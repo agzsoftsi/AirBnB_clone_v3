e65c48964ad1ac2c1ff98412e4ed4e2ec291a011
stuartses <stuart.ses@hotmail.com>
2020-04-28 15:26:46 -0500

    Add shebang and module documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+Initializes Blueprint and routes
+"""
+
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
