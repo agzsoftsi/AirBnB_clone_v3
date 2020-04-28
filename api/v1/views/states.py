8343044f5a024ae8e8b5f23b7d84e52bc6b0aded
agzsoftsi <agzsoftsi@gmail.com>
2020-04-28 21:39:27 +0000

    Create a new view for State objects that handles all default RestFul API actions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,52 +5,69 @@ Module View States
 
 """
 
-from models import storage
-from flask import jsonify, make_response
+from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
+from models import storage
+from models.state import State
 
 
-@app_views.route("/states")
-@app_views.route("/states/<state_id>")
-def view_states(state_id=None):
-    """ Return the list of states in json format by id
-
-        Argument:
-                state_id (str): id of object in State Class
-        Return:
-               json string with data of objects
-    """
-
-    states = storage.all("State").values()
-    states_dict = []
-
-    for state in states:
-        if state_id is None:
-            states_dict.append(state.to_dict())
-        elif state.id == state_id:
-            return jsonify(state.to_dict())
-
-    if state_id is not None:
-        return make_response(jsonify(error='Not found'), 404)
-
-    return jsonify(states_dict)
-
-@app_views.route("/states/<state_id>", methods=['DELETE'])
-def view_delete(state_id=None):
-    """ Delete state by id
-
-        Argument:
-                state_id (str): id of object in State Class
-        Return:
-               json string with status
-    """
-
-    states = storage.all("State").values()
-
-    for state in states:
-        if state.id == state_id:
-            state.delete()
-            return jsonify(state.to_dict()), 200
+@app_views.route('states', strict_slashes=False)
+def all_states():
+    """ GET Method to retrieve list of all State objects """
+    all_states = []
+    for state in storage.all('State').values():
+        all_states.append(state.to_dict())
+    return jsonify(all_states)
 
 
-    return make_response(jsonify(error='Not found'), 404)
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def retrieve_state(state_id):
+    """ GET Method to retrieve a particular State using id"""
+    try:
+        state = jsonify(storage.get('State', state_id).to_dict())
+        return state
+    except:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """ DELETE Method to delete a State """
+    state = storage.get('State', state_id)
+    if state:
+        state.delete()
+        storage.save()
+        return {}
+    abort(404)
+
+
+@app_views.route('/states', methods=['POST'],
+                 strict_slashes=False)
+def create_state():
+    """ POST Method to create a State """
+    state_name = request.get_json()
+    if not state_name:
+        abort(400, {'Not a JSON'})
+    elif 'name' not in state_name:
+        abort(400, {'Missing name'})
+    new_state = State(**state_name)
+    storage.new(new_state)
+    storage.save()
+    return new_state.to_dict(), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_state(state_id):
+    """ PUT Method to update a State """
+    update_attr = request.get_json()
+    if not update_attr:
+        abort(400, {'Not a JSON'})
+    my_state = storage.get('State', state_id)
+    if not my_state:
+        abort(404)
+    for key, value in update_attr.items():
+        setattr(my_state, key, value)
+    storage.save()
+    return my_state.to_dict()
